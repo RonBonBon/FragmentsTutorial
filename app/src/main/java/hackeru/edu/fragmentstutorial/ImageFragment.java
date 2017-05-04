a11cf77b691bdcabf0f2898ed99df861ad61af2b
RonBonBon <ron-aricha@013.net>
2017-05-04 20:56:28 +0300

    Fragments

--- app/src/main/java/hackeru/edu/fragmentstutorial/ImageFragment.java
+++ app/src/main/java/hackeru/edu/fragmentstutorial/ImageFragment.java
@@ -1,16 +1,12 @@
 package hackeru.edu.fragmentstutorial;
 
-
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-/**
- * A simple {@link Fragment} subclass.
- */
 public class ImageFragment extends Fragment {
 
 
@@ -18,12 +14,16 @@ public class ImageFragment extends Fragment {
         // Required empty public constructor
     }
 
+    ConstraintLayout layout;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_image, container, false);
+        View v = inflater.inflate(R.layout.fragment_image, container, false);
+        layout = (ConstraintLayout) v.findViewById(R.id.imageLayout);
+        return v;
     }
 
 }
